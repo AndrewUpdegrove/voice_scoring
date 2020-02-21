9d995079148fa37af80c228cd958d19046a467b1
AndrewUpdegrove <aupde3@gmail.com>
2020-02-20 23:58:42 -0500

    Got hello_audio working, and it identifies A440 from a piano

--- hello_audio.c
+++ hello_audio.c
@@ -1,14 +1,50 @@
 #include "aubio/aubio.h"
 #include "stdio.h"
-int main ()
+int main (int argc, char **argv)
 {
+  if (argc < 2){
+    printf("Not enough arguments\n");
+  }
+  uint_t samplerate = 0;
+  uint_t hop_size = 256;
+  uint_t win_size = 1024;
+  uint_t total_frames = 0, frames_read = 0;
+  fvec_t *hopper = new_fvec(hop_size);
 
-  printf("Hello /\n");
-  // 1. allocate some memory
-  uint_t n = 0; // frame counter
-  uint_t win_s = 1024; // window size
-  uint_t hop_s = win_s / 4; // hop size
-  uint_t samplerate = 44100; // samplerate
+  char_t *source_path = argv[1];
+  aubio_source_t* src  = new_aubio_source(source_path, samplerate, hop_size);
+  samplerate = aubio_source_get_samplerate(src);
 
-  fvec_t *input = new_fvec(hop_s); //input buffer
-  fvec_t *out = nev_fvec(1);
+  aubio_pitch_t *o = new_aubio_pitch("default", win_size, hop_size, samplerate);
+  //aubio_pitch_set_silence(o, .5);
+  fvec_t *out_buffer = new_fvec(hop_size);
+  fvec_t *pitch = new_fvec(1);
+
+  aubio_wavetable_t *wavetable = new_aubio_wavetable(samplerate, hop_size);
+  uint_t succ = aubio_wavetable_play(wavetable);
+  printf("Success? %d\n", succ);
+
+
+  do {
+    aubio_source_do(src, hopper, &frames_read);
+    aubio_pitch_do(o, hopper, pitch);
+    smpl_t freq =fvec_get_sample(pitch, 0);
+    printf("Pitch is: %f\n", pitch->data[0]);
+    aubio_wavetable_set_amp(wavetable, aubio_level_lin(hopper));
+    aubio_wavetable_set_freq(wavetable,freq);
+    aubio_wavetable_do(wavetable, hopper, out_buffer);
+    //fvec_print(hopper);
+    total_frames += frames_read;
+  } while (frames_read == hop_size);
+  printf("read %d frames at %dHz (%d blocks) from %s\n", total_frames, samplerate, total_frames / hop_size, source_path);
+
+  del_fvec(hopper);
+  del_fvec(pitch);
+  del_fvec(out_buffer);
+
+  del_aubio_source(src);
+  del_aubio_pitch(o);
+  del_aubio_wavetable(wavetable);
+
+
+}
--- test_pitch.c
+++ test_pitch.c
@@ -7,35 +7,46 @@ int main (int argc, char **argv)
   uint_t win_s = 1024; // window size
   uint_t hop_s = win_s / 4; // hop size
   uint_t samplerate = 0; // samplerate
-  unint_t n_frames = 0, read = 0;
+  uint_t n_frames = 0, read = 0;
 
-  unint_t err = 0;
+  uint_t err = 0;
   // create some vectors
   fvec_t *input = new_fvec (hop_s); // input buffer
-  fvec_t *out = new_fvec (1); // output candidates
+
 
   //declare new source
   char_t *source_path = argv[1];
-  aubio_source_t* s = new_aubio_soure(source_path_samplerate, hop_s);
+  aubio_source_t* s = new_aubio_source(source_path, samplerate, hop_s);
   if(!s){err = 1; goto beach;}
   if (samplerate == 0) samplerate = aubio_source_get_samplerate(s);
 
-
+  //hardcoding the size of the ouput vector, later we'll use a dynamically growing one?
+  fvec_t *out = new_fvec(8000); // output candidates
+  printf("%u\n", samplerate);
+  printf("%u\n", out->length);
+  fvec_t *out_2 = new_fvec(1);
   // create pitch object
   aubio_pitch_t *o = new_aubio_pitch ("default", win_s, hop_s, samplerate);
 
+  aubio_source_do(s, input, &read);
+  //fvec_print (input);
+  n_frames += read;
+  uint_t out_count = 0;
+  while(read == hop_s){
+    aubio_pitch_do(o, input, out_2);
+    out[out_count] = out_2[0];
+    out_count++;
+    printf("%u\n", out_count);
+    aubio_source_do(s, input, &read);
+    //fvec_print (input);
+    n_frames += read;
+  }
 
-  while (n < 100) {
-    // get `hop_s` new samples into `input`
-    // ...
-    // exectute pitch
-    aubio_pitch_do (o, input, out);
-    // do something with output candidates
-    // ...
-    n++;
-  };
-  // 3. clean up memory
+  printf("read %d frames at %dHz (%d blocks) from %s\n", n_frames, samplerate, n_frames / hop_s, source_path);
+  fvec_print(out);
+
   del_aubio_pitch (o);
+  del_aubio_source(s);
   del_fvec (out);
   del_fvec (input);
   aubio_cleanup ();
